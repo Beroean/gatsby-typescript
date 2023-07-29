b8f76a2381e6be65fd4d583327992d842bfe53fc
Mahmoud Shaar <m.shaar@gmail.com>
2023-07-29 10:51:37 -0500

    add text to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,14 @@ import Seo from "../components/seo";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <body style={{ marginBottom: "6px" }}>
+        Welcome to my personal website. I am using this website as a way to
+        experiment with new changes in the web development world outside of
+        work. I will also be posting occassional blog posts on topics that
+        interest me along with accompanying charts and data visualizations. The
+        midjourney AI generated the image below for me after I asked it to draw
+        a bazaar in my home city of Aleppo.
+      </body>
       <StaticImage
         alt="An AI generated picture of a bazaar during ramadan in my home city"
         src="../images/bazaar.png"
